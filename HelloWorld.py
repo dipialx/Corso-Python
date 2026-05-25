42fd3b079fe67a3d9e9b7ccceee7e02574cb34b1
alessandro.dipiazza <alessandro.dipiazza@ideoinnovation.com>
2026-05-25 10:55:04 +0200

    !1!

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+
+def saluta():
+    # Blocco indentato di 4 spazi appartenente alla funzione
+    print("Ciao!") 
+print("Benvenuto in Python.")
+
+# Blocco principale (fuori dalla funzione)
+print("Programma avviato")
+saluta()
+
