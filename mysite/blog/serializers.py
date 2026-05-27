15f37a2710aaaf1ab2f5bc666a8dd59d0d6ec9c1
alessandro.dipiazza <alessandro.dipiazza@ideoinnovation.com>
2026-05-27 11:19:35 +0200

    commit

--- mysite/blog/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Articolo
-
-class ArticoloSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Articolo
-        fields = ["id", "titolo", "contenuto", "pubblicato", "autore"]
